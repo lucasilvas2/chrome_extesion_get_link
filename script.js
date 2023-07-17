affecf0b333c315467847d7989d4ff3dde848381
Lucas Silva <lucasoliver1068@gmail.com>
2023-07-17 12:59:58 -0300

    feat: Convert link embed to link youtube

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ const getSrcIframe = () => {
 const botao = document.getElementById("btn_buscar");
 botao.addEventListener("click", async function () {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  let test = await chrome.scripting.executeScript({
+  let data = await chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: getSrcIframe,
   });
+  let url = convertEmbedToURL(data[0].result)
+
   let btn_link = document.getElementById("link");
-  btn_link.href = test[0].result;
+  btn_link.href = url;
   btn_link.classList.remove("disabled");
-  console.log(test[0].result);
+  console.log(data[0].result);
 });
+
+
+function getYouTubeVideoID(embeddedCode) {
+  const regex = /\/embed\/([^"?]+)/;
+  const match = embeddedCode.match(regex);
+  return match ? match[1] : null;
+}
+
+// Function to convert the YouTube video ID to a video URL
+function convertEmbedToURL(embeddedCode) {
+  const videoID = getYouTubeVideoID(embeddedCode);
+  if (videoID) {
+      return `https://www.youtube.com/watch?v=${videoID}`;
+  }
+  return null;
+}
